19b91a74d47896ecf6e966a1063bc1b3a92b98d9
seqenenra08 <juandavidalvarez08@gmail.com>
2025-01-07 14:45:18 -0500

    feat: implement basic task manager with add, list, and exit functionalities

--- main.php
+++ main.php
@@ -1,1 +1,59 @@
+<?php
+
+function addTask($input){
+    $task = $input[1];
+    if ($task == null) {
+        echo "Invalid task\n";
+        return;
+    }
+    $arraydata = [];
+    $arraydata['id'] = rand(1, 10000);
+    $arraydata['description'] = $task;
+    $arraydata['status'] = 'to do';
+    $arraydata['createAt'] = date('Y-m-d H:i:s');
+    $arraydata['updateAt'] = date('Y-m-d H:i:s');
+    $task = json_encode($arraydata);
+
+    $json = file_get_contents("task.json");
+    $data = json_decode($json, true);
+    $data[] = $task;
+    $json = json_encode($data);
+    file_put_contents("task.json", $json);
+}
+
+function listTasks($input){
+    $json = file_get_contents("task.json");
+    $data = json_decode($json);
+    foreach($data as $key => $task){
+        echo $key + 1 . ". " . $task . "\n";
+    }
+}
+
+function exitProgram(){
+    echo "Bye!\n";
+    return true;
+}
+
+function printInvalidCommand() {
+    echo "Invalid command\n";
+}
+
+function checkInput($input){
+    return match ($input[0]) {
+        "add" => addTask($input),
+        "list" => listTasks($input),
+        "exit" => exitProgram(),
+        default => printInvalidCommand(),
+    };
+}
+
+$exit  = false;
+echo "WELCOME TO TASK MANAGER \n";
+
+while(!$exit){
+    $inputUSer = readline();
+    $inputUSer = explode(" ", $inputUSer);
+    $exit = checkInput($inputUSer);
+}
+
 
